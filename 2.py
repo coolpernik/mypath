66b524901f15c1f2ca9eedb2e81093fd3d0f78b1
coolpernik <jinjifan0622@163.com>
2017-01-13 14:02:04 +0800

    add 2

---
+++ 2.py
@@ -0,0 +1,12 @@
+﻿import requests 
+from bs4 import BeautifulSoup
+
+r=requests.get(r"http://www.qiushibaike.com/")
+r.encoding="UTF8"
+
+t=BeautifulSoup(r.text,'html.parser')
+sts=t.select('.content')
+for st in sts:
+	print(st.text)
+input("press any key to finish")
+#http://xorx.cf/thread0806.php
